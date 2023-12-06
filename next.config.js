068356c49b8b49ab5574713700007769e67e2f87
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2023-12-06 19:34:40 +0300

    fix video projects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  transpilePackages: ["next-image-export-optimizer"],
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     largePageDataBytes: 180 * 1000,
   },
