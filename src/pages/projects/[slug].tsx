fe4eb6d9c5c713b8c9eed008e1eead02d37766a9
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2024-03-19 18:37:22 +0300

    fixes

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -9,25 +9,38 @@ import {
 } from "next";
 import he from "he";
 
-export const getStaticPaths = (async () => {
-  const res = await fetch("https://adm.bss-tv.com/projects/");
-  const data = await res.json();
+// export const getStaticPaths = (async () => {
+//   const res = await fetch("https://adm.bss-tv.com/projects/");
+//   const data = await res.json();
 
-  const paths = data.map((project: any) => ({
-    params: {
-      slug: project.DETAIL_PAGE_URL.replace("/projects/", "").slice(0, -1),
-    },
-  }));
-  console.log("====================================");
-  console.log(paths);
-  console.log("====================================");
-  return {
-    paths,
-    fallback: false,
-  };
-}) satisfies GetStaticPaths;
+//   const paths = data.map((project: any) => ({
+//     params: {
+//       slug: project.DETAIL_PAGE_URL.replace("/projects/", "").slice(0, -1),
+//     },
+//   }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }) satisfies GetStaticPaths;
 
-export const getStaticProps = (async (context) => {
+// export const getStaticProps = (async (context) => {
+//   const res = await fetch("https://adm.bss-tv.com/projects/");
+//   const data = await res.json();
+//   const project = data.filter((project: any) => {
+//     return (
+//       project.DETAIL_PAGE_URL.replace("/projects/", "").slice(0, -1) ===
+//       //@ts-ignore
+//       context.params?.slug
+//     );
+//   });
+
+//   return { props: { project }, revalidate: 60 };
+// }) satisfies GetStaticProps<{
+//   project: any;
+// }>;
+
+export const getServerSideProps = (async (context) => {
   const res = await fetch("https://adm.bss-tv.com/projects/");
   const data = await res.json();
   const project = data.filter((project: any) => {
@@ -38,14 +51,17 @@ export const getStaticProps = (async (context) => {
     );
   });
 
-  return { props: { project }, revalidate: 60 };
-}) satisfies GetStaticProps<{
+  return { props: { project } };
+}) satisfies GetServerSideProps<{
   project: any;
 }>;
 
 const ProjectPage = ({
   project,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  // const ProjectPage = ({
+  //   project,
+  // }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Head>
